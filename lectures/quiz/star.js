1674c8243aabd96ed82cb13215825253d226a702
sungmoo0608 <swingsm0608@gmail.com>
2025-01-10 14:03:23 +0900

    Javascript-learn-continue-quiz

---
+++ lectures/quiz/star.js
@@ -0,0 +1,187 @@
+'use strict';
+
+window.onload = function () {
+  /*
+   - '*'      document.write('*');
+   - 개행      document.write('<br>');
+   - space    document.write('&nbsp;'); 
+   - 가로선     document.write('<hr>');
+   */
+  //
+  /*
+    1] 정사각형
+    *****
+    *****
+    *****
+    *****
+    *****
+  */
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      document.write('*');
+    }
+    document.write('<br>');
+  }
+  document.write('<hr>');
+
+  /*
+    2] 삼각형1
+    *
+    **
+    ***
+    ****
+    *****
+  */
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < i + 1; j++) {
+      document.write('*');
+    }
+    document.write('<br>');
+  }
+  document.write('<hr>');
+
+  /*
+    3] 삼각형1-reverse
+    *****
+    ****
+    ***
+    **
+    *
+  */
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5 - i; j++) {
+      document.write('*');
+    }
+    document.write('<br>');
+  }
+  document.write('<hr>');
+
+  for (let i = 5; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      document.write('*');
+    }
+    document.write('<br>');
+  }
+  document.write('<hr>');
+
+  /*
+    4] 삼각형2
+        *
+       **
+      ***
+     ****
+    *****
+  */
+
+  for (let i = 0; i < 5; i++) {
+    for (let j = 4; j > i; j--) {
+      document.write('&nbsp;');
+    }
+    for (let k = 0; k < i + 1; k++) {
+      document.write('*');
+    }
+    document.write('<br>');
+  }
+  document.write('<hr>');
+
+  /*
+    5] 삼각형2-reverse
+    *****
+     ****
+      ***
+       **
+        *
+  */
+
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < i; j++) {
+      document.write('&nbsp;');
+    }
+    for (let k = 5; k > i; k--) {
+      document.write('*');
+    }
+    document.write('<br>');
+  }
+
+  document.write('<hr>');
+  /*
+    5] 정삼각형
+        *
+       ***
+      *****
+     ******* 
+    ********* 
+  */
+
+  for (let i = 0; i < 5; i++) {
+    for (let j = 5; j > i; j--) {
+      document.write('&nbsp;');
+    }
+    for (let k = 0; k < i * 2 + 1; k++) {
+      document.write('*');
+    }
+    document.write('<br>');
+  }
+  document.write('<hr>');
+
+  /*
+    5] 정삼각형-reverse
+    ********* 
+     ******* 
+      *****
+       ***
+        *
+  */
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < i; j++) {
+      document.write('&nbsp;');
+    }
+    for (let k = 5; k > i; k--) {
+      document.write('*');
+    }
+    for (let l = 4; l > i; l--) {
+      document.write('*');
+    }
+    document.write('<br>');
+  }
+
+  document.write('<hr>');
+  /*
+    5] 다이아몬드
+        *
+       ***
+      *****
+     ******* 
+    ********* 
+     ******* 
+      *****
+       ***
+        *
+  */
+
+  for (let i = 0; i < 5; i++) {
+    for (let j = 4; j > i; j--) {
+      document.write('&nbsp;');
+    }
+    for (let j = 0; j <= i; j++) {
+      document.write('*');
+    }
+    for (let j = 1; j <= i; j++) {
+      document.write('*');
+    }
+    document.write('<br>');
+  }
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j <= i; j++) {
+      document.write('&nbsp;');
+    }
+    for (let j = 4; j > i; j--) {
+      document.write('*');
+    }
+    for (let l = 3; l > i; l--) {
+      document.write('*');
+    }
+    document.write('<br>');
+  }
+  document.write('<hr>');
+};
